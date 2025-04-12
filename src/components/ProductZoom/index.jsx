8bfa8af0b24d7c8401293c076ca074a8a80a486f
Abhishek-baghel <abhishekbaghel.official02@gmail.com>
2025-04-12 20:58:04 +0530

    local update

--- src/components/ProductZoom/index.jsx
+++ src/components/ProductZoom/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import InnerImageZoom from "react-inner-image-zoom";
 import 'react-inner-image-zoom/lib/styles.min.css';
 import { Link } from 'react-router-dom';
@@ -9,76 +9,131 @@ import { Navigation } from 'swiper/modules';
 
 
 const ProductZoom = () => { 
+  const [slideIndex, setSlideIndex] = useState(0);
+  const zoomSliderBig = useRef();
+  const zoomSliderSml = useRef();
+
+  const goto = (index) => {
+    setSlideIndex(index);
+    zoomSliderSml.current.swiper.slideTo(index);
+    zoomSliderBig.current.swiper.slideTo(index);
+  };
+
+
   return (
     <>
       <div className="flex gap-3">
-        <div className="slider w-[20%]">
+        <div className="slider w-[15%]">
            <Swiper
+           ref={zoomSliderSml}
                               direction={"vertical"}
-                              slidesPerView={4}
-                              spaceBetween={10}
+                              slidesPerView={5}
+                              spaceBetween={0}
                               navigation={true}
                               modules={[Navigation]}
-                              className="zoomProductSliderThumbs h-[490px] overflow-hidden" /*sliderHome write in classname instead of mySwiper*/
+                              className="zoomProductSliderThumbs h-[500px] overflow-hidden" /*sliderHome write in classname instead of mySwiper*/
                           >
                             <SwiperSlide>
-                              <div className="item">
-                                <img src="https://res.cloudinary.com/demo/image/upload/sample.jpg"/>
+                            <div className={`item rounded-md overflow-hidden cursor-pointer group ${slideIndex === 0 ? 'opacity-1' : 'opacity-30'}`} onClick={() => goto(0)}>
+
+                                <img src="https://www.jiomart.com/images/product/original/443012090_green/quilted-zip-front-crop-gillet-model4-443012090_green-4-202309141728.jpg?im=Resize=(600,750)" className="w-full transition-all group-hover:scale-105"/>
                               </div>
                             </SwiperSlide>
 
                             <SwiperSlide>
-                              <div className="item">
-                                <img src="https://res.cloudinary.com/demo/image/upload/sample.jpg"/>
+                            <div className={`item rounded-md overflow-hidden cursor-pointer group ${slideIndex === 1 ? 'opacity-1' : 'opacity-30'}`} onClick={() => goto(1)}>
+                                <img src="https://www.jiomart.com/images/product/original/443012090_green/quilted-zip-front-crop-gillet-model5-443012090_green-5-202309141727.jpg?im=Resize=(75,94)" className="w-full transition-all group-hover:scale-105"/>
                               </div>
                             </SwiperSlide>
 
                             <SwiperSlide>
-                              <div className="item">
-                                <img src="https://res.cloudinary.com/demo/image/upload/sample.jpg"/>
+                            <div className={`item rounded-md overflow-hidden cursor-pointer group ${slideIndex === 2 ? 'opacity-1' : 'opacity-30'}`} onClick={() => goto(2)}>
+                                <img src="https://www.jiomart.com/images/product/original/443012090_green/quilted-zip-front-crop-gillet-model3-443012090_green-3-202309141728.jpg?im=Resize=(600,750)" className="w-full transition-all group-hover:scale-105"/>
                               </div>
                             </SwiperSlide>
 
                             <SwiperSlide>
-                              <div className="item">
-                                <img src="https://res.cloudinary.com/demo/image/upload/sample.jpg"/>
+                            <div className={`item rounded-md overflow-hidden cursor-pointer group ${slideIndex === 3 ? 'opacity-1' : 'opacity-30'}`} onClick={() => goto(3)}>
+                                <img src="https://www.jiomart.com/images/product/original/443012090_green/quilted-zip-front-crop-gillet-model-443012090_green-0-202309141728.jpg?im=Resize=(1000,1000)" className="w-full transition-all group-hover:scale-105"/>
                               </div>
                             </SwiperSlide>
 
                             <SwiperSlide>
-                              <div className="item">
-                                <img src="https://res.cloudinary.com/demo/image/upload/sample.jpg"/>
+                            <div className={`item rounded-md overflow-hidden cursor-pointer group ${slideIndex === 4 ? 'opacity-1' : 'opacity-30'}`} onClick={() => goto(4)}>
+                                <img src="https://www.jiomart.com/images/product/original/443012090_green/quilted-zip-front-crop-gillet-model2-443012090_green-2-202309141728.jpg?im=Resize=(600,750)"/>
                               </div>
                             </SwiperSlide>
 
                             <SwiperSlide>
-                              <div className="item">
-                                <img src="https://res.cloudinary.com/demo/image/upload/sample.jpg"/>
+                            <div className={`item rounded-md overflow-hidden cursor-pointer group ${slideIndex === 5 ? 'opacity-1' : 'opacity-30'}`} onClick={() => goto(5)}>
+                                <img src="https://www.jiomart.com/images/product/original/443012090_green/quilted-zip-front-crop-gillet-model6-443012090_green-6-202309141728.jpg?im=Resize=(600,750)"/>
                               </div>
                             </SwiperSlide>
 
-                            <SwiperSlide>
-                              <div className="item">
-                                <img src="https://res.cloudinary.com/demo/image/upload/sample.jpg"/>
-                              </div>
-                            </SwiperSlide>
-
-                            <SwiperSlide>
-                              <div className="item">
-                                <img src="https://res.cloudinary.com/demo/image/upload/sample.jpg"/>
-                              </div>
-                            </SwiperSlide>
-
+                            <SwiperSlide></SwiperSlide>
+                            
                           </Swiper>
         </div>
 
         
-        <div className="zoomContainer w-[80%] h-[500px] overflow-hidden">
+        <div className="zoomContainer w-[85%] h-[500px] overflow-hidden">
+        <Swiper
+                             
+                             ref={zoomSliderBig} 
+                             slidesPerView={1}
+                              spaceBetween={10}
+                              navigation={false}
+                          
+                          >
+      <SwiperSlide>
         <InnerImageZoom
         zoomType="hover"
         zoomScale={1}
-        src="https://res.cloudinary.com/demo/image/upload/sample.jpg"
-      />
+        src={"https://www.jiomart.com/images/product/original/443012090_green/quilted-zip-front-crop-gillet-model4-443012090_green-4-202309141728.jpg?im=Resize=(600,750)"
+        }/>
+      </SwiperSlide>
+
+      <SwiperSlide>
+  <InnerImageZoom
+    zoomType="hover"
+    zoomScale={1}
+    src={"https://www.jiomart.com/images/product/original/443012090_green/quilted-zip-front-crop-gillet-model5-443012090_green-5-202309141727.jpg?im=Resize=(600,750)"
+    }/>
+</SwiperSlide>
+
+      <SwiperSlide>
+        <InnerImageZoom
+        zoomType="hover"
+        zoomScale={1}
+        src={"https://www.jiomart.com/images/product/original/443012090_green/quilted-zip-front-crop-gillet-model3-443012090_green-3-202309141728.jpg?im=Resize=(600,750)"
+        }/>
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <InnerImageZoom
+        zoomType="hover"
+        zoomScale={1}
+        src={"https://www.jiomart.com/images/product/original/443012090_green/quilted-zip-front-crop-gillet-model-443012090_green-0-202309141728.jpg?im=Resize=(1000,1000)"
+        }/>
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <InnerImageZoom
+        zoomType="hover"
+        zoomScale={1}
+        src={"https://www.jiomart.com/images/product/original/443012090_green/quilted-zip-front-crop-gillet-model2-443012090_green-2-202309141728.jpg?im=Resize=(600,750)"
+        }/>
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <InnerImageZoom
+        zoomType="hover"
+        zoomScale={1}
+        src={"https://www.jiomart.com/images/product/original/443012090_green/quilted-zip-front-crop-gillet-model6-443012090_green-6-202309141728.jpg?im=Resize=(600,750)"
+        }/>
+      </SwiperSlide>
+      
+      </Swiper>
         </div>
       
    
